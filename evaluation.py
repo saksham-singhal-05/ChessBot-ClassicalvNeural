9f7bea0518fa1ae93dfd731a60cf629351ebf48e
Saksham Singhal <singhalsaksham2005@gmail.com>
2025-08-22 23:51:27 +0530

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -13,7 +13,6 @@ def evaluate_material(board):
     white_material = 0
     black_material = 0
     
-    # Count material for both sides
     for square in chess.SQUARES:
         piece = board.piece_at(square)
         if piece is not None:
@@ -30,26 +29,22 @@ def evaluate_check_safety(board):
     check_score = 0
     
     if board.is_check():
-        checking_side = not board.turn  # The side giving check
+        checking_side = not board.turn  
         king_square = board.king(board.turn)
         
         if king_square:
-            # Count attackers on the king
             attackers = board.attackers(checking_side, king_square)
             
-            # Check if the checking pieces are protected
             protected_attackers = 0
             unprotected_attackers = 0
             
             for attacker_square in attackers:
-                # Check if this attacking piece is defended
                 defenders = board.attackers(checking_side, attacker_square)
-                if len(defenders) > 1:  # More than just itself
+                if len(defenders) > 1:  
                     protected_attackers += 1
                 else:
                     unprotected_attackers += 1
             
-            # Bonus for protected checks, penalty for unprotected checks
             if checking_side == chess.WHITE:
                 check_score += protected_attackers * 30
                 check_score -= unprotected_attackers * 20
@@ -60,11 +55,9 @@ def evaluate_check_safety(board):
     return check_score
 
 def evaluate_position(board):
-    # Basic material evaluation
     material_score = evaluate_material(board)
     positional_score = 0
     
-    # Center control bonus
     center_squares = [chess.E4, chess.E5, chess.D4, chess.D5]
     for square in center_squares:
         piece = board.piece_at(square)
@@ -74,15 +67,12 @@ def evaluate_position(board):
             else:
                 positional_score -= 10
     
-    # Development bonus
     development_bonus = evaluate_development(board)
     positional_score += development_bonus
     
-    # **NEW: Check safety evaluation**
     check_bonus = evaluate_check_safety(board)
     positional_score += check_bonus
     
-    # King safety
     king_safety = evaluate_king_safety(board)
     positional_score += king_safety
     
@@ -91,7 +81,6 @@ def evaluate_position(board):
 def evaluate_development(board):
     development_score = 0
     
-    # White piece development
     if board.piece_at(chess.B1) != chess.Piece(chess.KNIGHT, chess.WHITE):
         development_score += 10
     if board.piece_at(chess.G1) != chess.Piece(chess.KNIGHT, chess.WHITE):
@@ -101,7 +90,6 @@ def evaluate_development(board):
     if board.piece_at(chess.F1) != chess.Piece(chess.BISHOP, chess.WHITE):
         development_score += 10
     
-    # Black piece development
     if board.piece_at(chess.B8) != chess.Piece(chess.KNIGHT, chess.BLACK):
         development_score -= 10
     if board.piece_at(chess.G8) != chess.Piece(chess.KNIGHT, chess.BLACK):
@@ -117,12 +105,10 @@ def evaluate_king_safety(board):
     """Evaluate king safety"""
     safety_score = 0
     
-    # Check both kings
     white_king = board.king(chess.WHITE)
     black_king = board.king(chess.BLACK)
     
     if white_king:
-        # Count enemy pieces near white king
         enemy_near_white = 0
         for square in chess.SQUARES:
             if chess.square_distance(square, white_king) <= 2:
@@ -132,7 +118,6 @@ def evaluate_king_safety(board):
         safety_score -= enemy_near_white * 5
     
     if black_king:
-        # Count enemy pieces near black king
         enemy_near_black = 0
         for square in chess.SQUARES:
             if chess.square_distance(square, black_king) <= 2:
